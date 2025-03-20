876eeb817c2a0f17f3cc92a74316788e2c56377f
typhosj <157098130+typhosj@users.noreply.github.com>
2025-03-20 06:50:03 +0100

    Update mocha.setup.js

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -7,9 +7,9 @@ process.on("unhandledRejection", (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 //const sinonChai = require("sinon-chai");
-const chaiAsPromised = require("chai-as-promised");
-const { should, use } = require("chai");
+//const chaiAsPromised = require("chai-as-promised");
+//const { should, use } = require("chai");
 
 should();
 //use(sinonChai);
-use(chaiAsPromised);
+//use(chaiAsPromised);
